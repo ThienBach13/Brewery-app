83b5acb83423e900e9483f95db03f1b830d3df93
CastAway13 <hoangthien130400@gmail.com>
2024-02-07 23:15:45 +0200

    Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+import { Brewery } from "./misc/type";
 
 const App = () => {
+  const [breweries, setBreweries] = useState<Brewery[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.openbrewerydb.org/v1/breweries')
+      .then((response) => response.json())
+      .then((breweries) => setBreweries(breweries));
+  }, []);
+
+
   return (
-    <div>App</div>
+    <div>
+    {breweries.map(brewery => (
+      <div key={brewery.id}>
+        <h2>{brewery.name}</h2>
+        <p>City: {brewery.city}</p>
+        <p>Country: {brewery.country}</p>
+        <p>Phone: {brewery.phone}</p>
+      </div>
+    ))}
+  </div>
   )
 }
 
---
+++ src/misc/type.tsx
@@ -0,0 +1,7 @@
+export type Brewery = {
+    id: string;
+    name: string;
+    city: string;
+    country: string;
+    phone: string;
+  };
